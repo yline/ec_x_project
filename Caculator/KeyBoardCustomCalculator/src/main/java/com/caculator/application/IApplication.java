db1e8866ce724ba945c33a98390d2296bd36e749
yline <957339173@qq.com>
2017-08-20 13:57:12 +0800

    this:计算器 数据提交 next:continue

---
+++ Caculator/KeyBoardCustomCalculator/src/main/java/com/caculator/application/IApplication.java
@@ -0,0 +1,8 @@
+package com.caculator.application;
+
+import com.yline.application.BaseApplication;
+
+public class IApplication extends BaseApplication
+{
+	public static final String TAG = "Calculator";
+}
