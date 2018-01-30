a38b9b400722cfa2f8749d6c72598bc8f5722ecd
yline <957339173@qq.com>
2018-01-30 13:53:31 +0800

    this:FileManager删除 next:continue

--- FileInfos/FileManager/src/main/java/com/file/chooser/activity/IApplication.java
+++
@@ -1,7 +0,0 @@
-package com.file.chooser.activity;
-
-import com.yline.application.BaseApplication;
-
-public class IApplication extends BaseApplication {
-    public static final String TAG = "FileChooser";
-}
