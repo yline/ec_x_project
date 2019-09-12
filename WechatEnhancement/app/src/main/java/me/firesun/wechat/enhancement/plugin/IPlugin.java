eb2877c84a65b1b9a5e5f6f476607b6d42dbe332
ylineWork <linjiang@kjtpay.local>
2019-09-12 17:47:30 +0800

    this:删除 微信插件的 两个工程

--- WechatEnhancement/app/src/main/java/me/firesun/wechat/enhancement/plugin/IPlugin.java
+++
@@ -1,7 +0,0 @@
-package me.firesun.wechat.enhancement.plugin;
-
-import de.robv.android.xposed.callbacks.XC_LoadPackage;
-
-public interface IPlugin {
-    public void hook(XC_LoadPackage.LoadPackageParam lpparam);
-}
--- XposedWechatHelper/wechathelper/src/main/java/com/wuxiaosu/wechathelper/utils/Constant.java
+++
@@ -1,7 +0,0 @@
-package com.wuxiaosu.wechathelper.utils;
-
-public class Constant {
-
-    public static final String PRO_FILE = "/storage/emulated/0/wechathelper/" + "stetting_config.pro";
-
-}
